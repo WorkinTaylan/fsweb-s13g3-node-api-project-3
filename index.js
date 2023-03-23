c6b8bf0c28b65b8bc780f8183a2e20c57aa30aff
WorkinTaylan <acikgozta@gmail.com>
2023-03-23 12:06:02 +0300

    env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // require your server and launch it
 const server = require("./api/server");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, ()=>{
     console.log(`Server running on ${port}`)
